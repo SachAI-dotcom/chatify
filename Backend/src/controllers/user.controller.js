a3b262472bc8f9a37b539cee49d89ba7ffc1f726
sachin <sachinkumar59756@gmail.com>
2026-01-07 04:20:38 +0530

    fixed bugs

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -5,28 +5,36 @@ import ApiError from "../utils/ApiError.js";
 import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
 
-const generateRefreshTokenAndAccessToken = asyncHandler(async (req, res, next) => {
-  const user = req.user;
-  if (!user) {
-    return next(new ApiError(401, "Cant find user while generating tokens"));
+const generateRefreshTokenAndAccessToken = asyncHandler(
+  async (req, res, next) => {
+    const user = req.user;
+    if (!user) {
+      return next(new ApiError(401, "Cant find user while generating tokens"));
+    }
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    const options = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken },
+          "Tokens generated successfully"
+        )
+      );
   }
-  const accessToken = user.generateAccessToken();
-  const refreshToken = user.generateRefreshToken();
-  
-  user.refreshToken = refreshToken;
-  await user.save({ validateBeforeSave: false });
-  
-  const options = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-  };
-  
-  return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(new ApiResponse(200, { accessToken, refreshToken }, "Tokens generated successfully"));
-});
+);
 const registerUser = asyncHandler(async (req, res, next) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
@@ -34,16 +42,24 @@ const registerUser = asyncHandler(async (req, res, next) => {
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await User.create({ username, email, password: hashedPassword });
-  const createdUser = await User.findById(user._id).select("-password -refreshToken -accessToken");
-  return new ApiResponse(201, createdUser, "User registered successfully").send(res);
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -accessToken"
+  );
+  return new ApiResponse(201, createdUser, "User registered successfully").send(
+    res
+  );
 });
 
 const loginUser = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return next(new ApiError(400, "All fields are required"));
+  const { email, password, username } = req.body;
+  if ((!email && !username) || !password) {
+    return next(
+      new ApiError(400, "Email or username and password are required")
+    );
   }
-  const user = await User.findOne({ email });
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
   if (!user) {
     return next(new ApiError(401, "cant find user during login"));
   }
@@ -53,33 +69,45 @@ const loginUser = asyncHandler(async (req, res, next) => {
   }
   const accessToken = user.generateAccessToken();
   const refreshToken = user.generateRefreshToken();
-  
+
   user.refreshToken = refreshToken;
   await user.save({ validateBeforeSave: false });
-  
-  const loggedInUser = await User.findById(user._id).select("-password -refreshToken -accessToken");
-  
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken -accessToken"
+  );
+
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
   };
-  
+
   return res
     .status(200)
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
-    .json(new ApiResponse(200, { user: loggedInUser, accessToken, refreshToken }, "User logged in successfully"));
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
 });
 const logOut = asyncHandler(async (req, res) => {
   const user = req.user;
   const id = user._id;
-  await User.findByIdAndUpdate(id, {
-    $unset: {
-      refreshToken: 1,
+  await User.findByIdAndUpdate(
+    id,
+    {
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,
     }
-  }, {
-    new: true,
-  });
+  );
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
